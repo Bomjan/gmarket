b91f35c1f3c550f459ab447deb6e0ff19348a126
Bomjan <sundrabomjan@gmail.com>
2026-06-02 01:48:34 +0600

    struct tags for json

--- backend/internal/types/types.go
+++ backend/internal/types/types.go
@@ -2,13 +2,13 @@ package types
 
 type Student struct {
 	Id    int64
-	Name  string `validate:"required"`
-	Email string `validate:"required"`
-	Age   int    `validate:"required"`
+	Name  string `json:"name" validate:"required"`
+	Email string `json:"email" validate:"required"`
+	Age   int    `json:"age" validate:"required"`
 }
 
 type Product struct {
-	Id    int64
-	Name  string  `validate:"required"`
-	Price float64 `validate:"required"`
+	Id    int64   `json:"id" validate:"required"`
+	Name  string  `json:"name" validate:"required"`
+	Price float64 `json:"price" validate:"required"`
 }
